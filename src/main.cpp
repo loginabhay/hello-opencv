07b033d6dc4603da6b937130d73912927fcf182c
Suraj Pai <39863294+suraj-pai@users.noreply.github.com>
2019-07-03 02:15:53 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include "img.h"
+#include "img.hpp"
 
 int main()
 {
+    // The image hpp file contains the definition of the function ReadAndDisplayImage. 
     ReadAndDisplayImage();
     return 0;
 }
