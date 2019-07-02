8f0c2220e92a9412117a23181f87dd9cb06c0a1a
Suraj Pai <39863294+suraj-pai@users.noreply.github.com>
2019-07-03 02:17:59 +0530

    Update img.hpp

--- src/img.hpp
+++ src/img.hpp
@@ -1,3 +1,7 @@
-#pragma once
+// Check to see if the header file has already been imported
+#ifndef HEADER_GUARD
+#define HEADER_GUARD
+#endif
 
+// Function Declaration for ReadAndDisplayImage, the definition will be made in the img.cpp file
 void ReadAndDisplayImage();
